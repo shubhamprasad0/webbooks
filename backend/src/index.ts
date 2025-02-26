803b717f294532dec76392f0fe66485265908a44
Shubham Prasad <shubhamprasad63@gmail.com>
2025-02-26 16:49:12 +0530

    Fix db tls config for render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import typeDefs from "./graphql/typeDefs.js";
 import resolvers from "./graphql/resolvers.js";
+import "dotenv/config";
 
 const server = new ApolloServer({
   typeDefs,
@@ -13,7 +14,7 @@ const server = new ApolloServer({
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: +process.env.PORT || 4000 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
