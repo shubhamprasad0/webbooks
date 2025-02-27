1a2b3b230bf5e722083c88f9f2018e74a4ed8cbf
Shubham Prasad <shubhamprasad63@gmail.com>
2025-02-28 01:11:53 +0530

    Refactor

--- frontend/src/components/authors.tsx
+++ frontend/src/components/authors.tsx
@@ -7,32 +7,8 @@ import {
 } from "@/components/ui/card";
 
 import AuthorsTable from "./authors-table";
-import { gql, useQuery } from "@apollo/client";
-
-export const GET_AUTHORS = gql`
-  query GetAuthors {
-    authors {
-      authors {
-        id
-        name
-        biography
-        bornDate
-      }
-    }
-  }
-`;
 
 const Authors = () => {
-  const { loading, error, data } = useQuery(GET_AUTHORS);
-
-  if (loading) {
-    return `Loading...`;
-  }
-
-  if (error) {
-    return `Error ${error.message}`;
-  }
-
   return (
     <Card x-chunk="dashboard-06-chunk-0">
       <CardHeader>
